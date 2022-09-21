23623f2e9ef79d0b2a5bffbf93b857ba51fbd962
Therny <anton.90k@gmail.com>
2022-09-21 23:26:49 +0300

    ex11

--- HomeWork/Ex11.py
+++ HomeWork/Ex11.py
@@ -1,1 +1,11 @@
-print(1)
+import requests
+import pytest
+
+class TestAssertsCookie:
+    def test_assert_cookie(self):
+        url = "https://playground.learnqa.ru/api/homework_cookie"
+        response = requests.get(url)
+        cookies = dict(response.cookies)
+        print(cookies)
+
+        assert cookies == {'HomeWork': 'hw_value'}, f"Cookie '{cookies}' is not assert with print(cookie): 'HomeWork': 'hw_value'"
